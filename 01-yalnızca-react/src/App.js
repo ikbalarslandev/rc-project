7adb1247f8c99e9ca507eafaf85e9abaa00a9af3
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-05-01 12:03:49 +0300

    create components using createElement

--- 01-yalnızca-react/src/App.js
+++ 01-yalnızca-react/src/App.js
@@ -1,6 +1,11 @@
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement("div", {}, [
+      React.createElement("h1", {}, "Luna"),
+      React.createElement("h2", {}, "Dog"),
+      React.createElement("h2", {}, "Havanese"),
+    ]),
   ]);
 };
 
